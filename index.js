8c7233aa835f2b15def07b83a783b773ede0940b
Shoriful-Islam25 <codefor75@gmail.com>
2022-11-09 20:21:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+const Port = process.env.Port || 5000;
+
+const allCourses = require("./Data/courses.json");
+
+app.get("/", (req, res) => {
+  res.send("The server in running");
+});
+app.get("/allcourses", (req, res) => {
+  res.send(allCourses);
+});
+
+app.get("/coursedetails/:id", (req, res) => {
+  const id = req.params.id;
+  const course = allCourses.find((courses) => courses.id == id);
+  res.send(course);
+});
+
+app.listen(Port, () => {
+  console.log("api is running", Port);
+});
